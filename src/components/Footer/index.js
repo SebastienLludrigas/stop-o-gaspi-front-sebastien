6fc1e6359a80defd57523e51d0e3df0cdc3e0e2b
ptlu79 <grbroyer@gmail.com>
2020-07-04 11:55:04 +0200

    Footer- with Link and responsive all size

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 // == Import npm
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // == Import
 import Phone from '@bit/feathericons.react-feather.phone';
@@ -10,34 +11,51 @@ import './footer.scss';
 // == Composant
 const Footer = () => (
   <footer>
-    <div>
+
+    <div className="aboutUs">
       <h3 className="footerTitle">Stop O'Gaspi :</h3>
       <ul>
-        <li className="footerHoover"> <a href="#">Qui sommes nous ?</a> </li>
-        <li className="footerHoover"> <a href="#">Nous rejoindre</a> </li>
+        <Link to="/advice-sheet/about" className="footerHoover">
+          <li>Qui sommes nous ?</li>
+        </Link>
+        <Link to="/join-us" className="footerHoover">
+          <li>Nous rejoindre</li>
+        </Link>
       </ul>
-
     </div>
+
     <div className="contact">
       <h3 className="footerTitle">Contact :</h3>
-
       <ul className="lienContact">
-        <li className="footerHoover"><a href="#"> <Phone size="15" /> </a> </li>
-        <li className="footerHoover"><a href="#"> <Mail size="15" /> </a> </li>
-        <li className="footerHoover"><a href="#">FAQ</a></li>
+        <Link to="/advice-sheet/about" className="footerHoover">
+          <li> <Phone size="15" /> </li>
+        </Link>
+        <Link to="/advice-sheet/about" className="footerHoover">
+          <li> <Mail size="15" /> </li>
+        </Link>
+        <Link to="/advice-sheet/about" className="footerHoover">
+          <li>FAQ</li>
+        </Link>
       </ul>
-      <p>2020 @copyright Stop O'Gaspi | <a className="footerHoover" href="#">Mentions Légales</a></p>
+
+      <p>2020 @copyright Stop O'Gaspi | 
+        <Link to="/advice-sheet/about" className="footerHoover">
+          <a>Mentions Légales</a>
+        </Link>
+      </p>
 
     </div>
-    <div className="blogCommunautiy">
-      <h3 className="footerHoover footerTitle"><a href="#">Blog</a></h3>
-      <ul>
-        <li className="footerHoover"><a href="#" className="footerTitle">Communauté</a></li>
-      </ul>
+    <div className="blogCommunauty">
+      <Link to="/advice-sheet/about" className="footerHoover">
+        <li className="footerHoover footerTitle"><a>Blog</a></li>
+      </Link>
+      <Link to="/advice-sheet/about" className="footerHoover">
+        <li className="footerHoover"><a className="footerTitle">Communauté</a></li>
+      </Link>
     </div>
   </footer>
 );
-// “/about”“/legal-mentions”	“/sitemap
+
 
 // == Export
 export default Footer;
