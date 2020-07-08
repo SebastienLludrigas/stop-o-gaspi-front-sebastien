ce02ca3e6902b95a756645645452f40bfe8f6949
ptlu79 <grbroyer@gmail.com>
2020-07-08 13:59:55 +0200

    scanProduct static ok

--- src/components/ScanProduct/index.js
+++ src/components/ScanProduct/index.js
@@ -8,17 +8,29 @@ import './scanProduct.scss';
 
 // == Composant
 const ScanProduct = () => {
-  const pantryText=useSpring({ marginLeft: 0, from: { marginLeft: 500 } });
+  const scanText=useSpring({ marginLeft: 0, from: { marginLeft: 500 } });
   return (
     <div className="scanPage">
       <animated.div
-        style={pantryText}
+        style={scanText}
         config={{ delay: 2000, duration: 2000 }}
       >
-        dd
-        
+        <div className="right_scanPage">
+
+          <div className="scanInput">
+            <h2>je scanne mon code barre :</h2>
+            <div className="btn_scan_direct">+</div>
+          </div>
+
+          <div className="manualInput">
+            <h2>je saisie mon code barre :</h2>
+            <input type="text" name="" required="" />
+            <div className="btn_validate_barcode">Valider</div>
+          </div>
+          
+        </div>
       </animated.div>
-      
+
     </div>
   );
 }
