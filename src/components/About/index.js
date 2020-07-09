15c6a264d01349f6333fe38be29259a7b182cbc9
ptlu79 <grbroyer@gmail.com>
2020-07-09 16:14:48 +0200

    about page depart

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// == Import
+import './about.scss';
+
+// == Composant
+const About = () => (
+  <p>fdg</p>
+);
+
+// == Export
+export default About;
