9e8db26bd0947c3f782a4f6abd68535d069967b6
ptlu79 <grbroyer@gmail.com>
2020-07-09 23:35:41 +0200

    page about without device mobil view

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,69 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { useSpring, animated as a} from 'react-spring';
 
 // == Import
 import './about.scss';
+import laurie from 'src/assets/image/Blanche-neige.jpg';
+import sebastien from 'src/assets/image/Prof.png';
+import aurelien from 'src/assets/image/Timide.png';
+import greg from 'src/assets/image/Grincheux.png';
+import alexis from 'src/assets/image/Dormeur.png';
 
 // == Composant
-const About = () => (
-  <p>fdg</p>
-);
+const About = () => {
 
+  const effectAbout= useSpring({ marginTop: 0, from: { marginTop: 500 } });
+
+  return (
+    <div className="about_page">
+      <a.div
+        className="about_title"
+        style={effectAbout}
+        config={{ delay: 1, duration: 50000 }}
+      >
+        <h2>
+          L'équipe de Stop O' Gaspi :
+        </h2>
+      </a.div>
+      <img className="photo_laurie" alt="" src={laurie} />
+      <ul className="about_laurie ul_about">
+        <li>LAURIE</li>
+        <li>Product Owner</li>
+        <li>Dev Back</li>
+
+      </ul>
+      <img className="photo_sebastien" alt="" src={sebastien} />
+      <ul className="about_sebastien ul_about">
+        <li>SEBASTIEN</li>
+        <li>Lead Front</li>
+        <li>Dev Front</li>
+
+      </ul>
+      <img className="photo_aurelien" alt="" src={aurelien} />
+      <ul className="about_aurelien ul_about">
+        <li>AURELIEN</li>
+        <li>Lead Back</li>
+        <li>Dev Back</li>
+
+      </ul>
+      <img className="photo_greg" alt="" src={greg} />
+      <ul className="about_greg ul_about">
+        <li>GREG</li>
+        <li>Git Master</li>
+        <li>Dev Front</li>
+
+      </ul>
+      <img className="photo_alexis" alt="" src={alexis} />
+      <ul className="about_alexis ul_about">
+        <li>ALEXIS</li>
+        <li>Scrum Master</li>
+        <li>Dev Front</li>
+
+      </ul>
+
+      <h2 className="tous">tous issue d'une formation FULLSTACK</h2>
+    </div>
+  );
+};
 // == Export
 export default About;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -11,37 +11,20 @@ import './main.scss';
 const Main = ({ handleDatas, sendDatas }) => (
   <div className="main">
     <h1 className="titleMain">Bienvenue à toi Gaspi Hunter.</h1>
-    <Fridge />
-    <p className="textMain">
-      Toi aussi tu te dis que gâcher c’est nul?
-      <br />
-      <br />
-      Tu te fais avoir à chaque fois que tu fais les courses,
-      tu achètes plein de trucs que tu oublies de manger ?
-      <br /><br />
-      Et bien nous aussi, c’est humain.
-      <br /><br />
-      Heureusement on a réfléchi et trouvé une solution à ce problème afin de dire STOP’O’GASPI.
-    </p>
-    <button
-      className="buttonDatas"
-      type="button"
-      onClick={() => {
-        handleDatas();
-      }}
-    >
-      Afficher les données
-    </button>
-    <button
-      className="buttonDatas2"
-      type="button"
-      onClick={() => {
-        sendDatas();
-      }}
-    >
-      Envoyer des données
-    </button>
-
+    <div className="main_left"><Fridge /></div>
+    <div className="main_right">
+      <p className="textMain">
+        Toi aussi tu te dis que gâcher c’est nul?
+        <br />
+        <br />
+        Tu te fais avoir à chaque fois que tu fais les courses,
+        tu achètes plein de trucs que tu oublies de manger ?
+        <br /><br />
+        Et bien nous aussi, c’est humain.
+        <br /><br />
+        Heureusement tu viens de trouvé une solution à ce problème afin de dire STOP’O’GASPI.
+      </p>
+    </div>
   </div>
 );
 
