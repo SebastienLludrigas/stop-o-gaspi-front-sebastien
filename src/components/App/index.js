e2e89fa5a0b14a62dacb7aa4ddddf6faf7b5ab12
SebastienLludrigas <sebastienlludrigas@gmail.com>
2020-07-17 10:37:07 +0200

    update

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -26,7 +26,7 @@ import './styles.scss';
 const App = () => (
   <div className="app">
     <Header />
-    <Switch >
+    <Switch>
       <Route exact path="/" component={Main} />
       <Route forceRefresh path="/pantry" component={Pantry} />
       <Route path="/connexion" component={Connexion} />
