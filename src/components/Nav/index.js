ca844fadf20a1beb0a7585d58181a3fa08aca3f6
Sebastien Lludrigas <sebastienlludrigas@gmail.com>
2020-08-18 07:26:41 +0200

    first modifs of new repo

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,6 @@
 // == Import npm
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -25,9 +25,8 @@ const Nav = ({
       </ul>
     </nav>
 
-
     <nav id="column-menu">
-    
+
       <ul className={classNames('column-menu-content', { active: cross })}>
 
         <li className="nav">
@@ -68,8 +67,7 @@ const Nav = ({
         {isLogged && (
           <li
             className="nav"
-            onClick={logOut
-            }
+            onClick={logOut}
           >
             Déconnexion
           </li>
@@ -77,15 +75,43 @@ const Nav = ({
 
         <div className="social-media">
           <div className="social-media-container">
-            <li><NavLink to="/twitter" title="twitter"><span className="fab fa-twitter" onClick={() => { handleBurger(); }} /></NavLink></li>
-            <li><NavLink to="/facebook" title="facebook"><span className="fab fa-facebook" onClick={() => { handleBurger(); }} /></NavLink></li>
-            <li><NavLink to="/instagram" title="instagram"><span className="fab fa-instagram" onClick={() => { handleBurger(); }} /></NavLink></li>
-            <li><NavLink to="/snapchat" title="snapchat"><span className="fab fa-snapchat" onClick={() => { handleBurger(); }} /></NavLink></li>
+            <li><NavLink to="/twitter" title="twitter">
+              <span
+                className="fab fa-twitter"
+                onClick={() => {
+                  handleBurger();
+                }}
+              /></NavLink>
+            </li>
+            <li><NavLink to="/facebook" title="facebook">
+              <span
+                className="fab fa-facebook"
+                onClick={() => {
+                  handleBurger();
+                }}
+              /></NavLink>
+            </li>
+            <li><NavLink to="/instagram" title="instagram">
+              <span
+                className="fab fa-instagram"
+                onClick={() => {
+                  handleBurger();
+                }}
+              /></NavLink>
+            </li>
+            <li><NavLink to="/snapchat" title="snapchat">
+              <span
+                className="fab fa-snapchat"
+                onClick={() => {
+                  handleBurger();
+                }}
+              /></NavLink>
+            </li>
             <br />
           </div>
         </div>
       </ul>
-    
+
     </nav>
 
     {isLogged && <Burger handleBurger={handleBurger} cross={cross} />}
